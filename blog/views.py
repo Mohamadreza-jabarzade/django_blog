9a74ba3849766e42b7cb9bf235559047869cfdfe
Mohamadreza <mr.jabarzade2005@gmail.com>
2025-05-23 02:49:16 +0330

    html source added to template folder but base.html not fix...

---
+++ blog/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+from blog.models import Post
+
+
+# Create your views here.
+def home(request):
+    posts = Post.objects.all().order_by('-created_at')
+    return render(request, 'home.html', {'posts': posts})
