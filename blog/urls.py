36c10dd4fe10a3b275a1e85ced35544b60b1e595
Mohamadreza <mr.jabarzade2005@gmail.com>
2025-05-23 04:13:15 +0330

    base.html and templates added and slider created

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('posts/', views.posts, name='posts'),
+    path('contact/', views.contact, name='contact'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from blog.models import Post
+from django.shortcuts import render
+from .models import Post  # فرض بر اینکه مدل Post داری
 
-
-# Create your views here.
 def home(request):
-    posts = Post.objects.all().order_by('-created_at')
-    return render(request, 'home.html', {'posts': posts})
+    top_posts = Post.objects.order_by('created_at')[:3]  # یا هر شرط دلخواه
+    return render(request, 'home.html', {'top_posts': top_posts})
+
+def about(request):
+    return render(request, 'about.html')
+
+def posts(request):
+    # اگر دوتا صفحه متفاوت می‌خوای می‌تونی تغییر بدی
+    posts = Post.objects.all()
+    return render(request, 'posts.html', {'posts': posts})
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'post_detail.html', {'post': post})
