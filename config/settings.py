54c0e0136d94227b819b241f22939db5098e4830
Mohamadreza <mr.jabarzade2005@gmail.com>
2025-05-24 17:39:26 +0330

    change admin panel date to shamsi

--- config/settings.py
+++ config/settings.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     'blog',
     'shop',
     'payment',
+    'jalali_date',
 ]
 
 MIDDLEWARE = [
--- payment/admin.py
+++ payment/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from .models import Purchase
+from jalali_date.admin import ModelAdminJalaliMixin
+from jalali_date import datetime2jalali
 
 @admin.register(Purchase)
-class PurchaseAdmin(admin.ModelAdmin):
-    list_display = ('tracking_code', 'email', 'phone', 'product_name', 'created_at')
-    search_fields = ('email', 'tracking_code', 'phone', 'product_name')
+class PurchaseAdmin(ModelAdminJalaliMixin, admin.ModelAdmin):
+    list_display = ['tracking_code', 'email', 'phone', 'product_name', 'shamsi_date']
+
+    def shamsi_date(self, obj):
+        return datetime2jalali(obj.created_at).strftime('%Y/%m/%d')
+    shamsi_date.short_description = 'تاریخ'
